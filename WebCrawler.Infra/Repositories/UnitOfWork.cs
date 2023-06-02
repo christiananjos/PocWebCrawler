89b744503cbd8ebdc012ad64b6a3587ea5a110d6
Christian_Anjos <christiananjos@hotmail.com>
2023-06-02 16:53:54 -0300

    implementado UnitOfWork no projeto Infra

---
+++ WebCrawler.Infra/Repositories/UnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using WebCrawler.Domain.Interfaces;
+using WebCrawler.Infra.Context;
+
+namespace WebCrawler.Infra.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context) => _context = context;
+
+        public async Task Commit() => await _context.SaveChangesAsync();
+    }
+}
