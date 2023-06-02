eac25e9ae473e3c1effadafb61ac87b68b2b0f93
Christian_Anjos <christiananjos@hotmail.com>
2023-06-02 16:49:44 -0300

    Implementado Repository e contexto para o banco

---
+++ WebCrawler.Infra/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebCrawler.Domain.Models;
+
+namespace WebCrawler.Infra.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
---
+++ WebCrawler.Infra/Repositories/Repository.cs
@@ -0,0 +1,32 @@
+﻿using WebCrawler.Domain.Interfaces;
+using WebCrawler.Domain.Models;
+using WebCrawler.Infra.Context;
+
+namespace WebCrawler.Infra.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+    {
+        protected readonly AppDbContext _context;
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public virtual TEntity GetById(Guid id)
+        {
+            var query = _context.Set<TEntity>().Where(e => e.Id == id);
+
+            if (!query.Any()) return null;
+
+            return query.FirstOrDefault();
+        }
+        public virtual IEnumerable<TEntity> GetAll()
+        {
+            var query = _context.Set<TEntity>();
+
+            if (query.Any()) return query.ToList();
+
+            return new List<TEntity>();
+        }
+        public virtual void Save(TEntity entity) => _context.Set<TEntity>().Add(entity);
+    }
+}
