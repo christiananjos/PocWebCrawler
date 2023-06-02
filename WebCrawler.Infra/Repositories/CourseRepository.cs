351ba1e17212f9ecbe39ab306bcd35d2fde48fbe
Christian_Anjos <christiananjos@hotmail.com>
2023-06-02 16:52:26 -0300

    implementado CourseRepository

---
+++ WebCrawler.Infra/Repositories/CourseRepository.cs
@@ -0,0 +1,28 @@
+﻿using WebCrawler.Domain.Models;
+using WebCrawler.Infra.Context;
+
+namespace WebCrawler.Infra.Repositories
+{
+    public class CourseRepository : Repository<Course>
+    {
+        public CourseRepository(AppDbContext context) : base(context)
+        { }
+
+        public override Course GetById(Guid id)
+        {
+            var query = _context.Set<Course>().Where(e => e.Id == id);
+
+            if (query.Any())
+                return query.First();
+
+            return null;
+        }
+
+        public override IEnumerable<Course> GetAll()
+        {
+            var query = _context.Set<Course>();
+
+            return query.Any() ? query.ToList() : new List<Course>();
+        }
+    }
+}
