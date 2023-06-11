8b87f7e6c6f3fc50a27823fd147a17e2f12417bc
Christian_Anjos <christiananjos@hotmail.com>
2023-06-11 20:37:13 -0300

    adicionado dto e ConnectionStrings

---
+++ WebCrawler.API/DTOs/CourseDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebCrawler.API.DTOs
+{
+    public class CourseDTO
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Title { get; private set; }
+
+        [Required]
+        public string Description { get; private set; }
+
+        [Required]
+        public string Hours { get; private set; }
+
+        [Required]
+        public string TeacherName { get; private set; }
+    }
+}
